bac9fb36de4f96550d4549677040bb355316fd23
Galen G <github@mastrchef.rocks>
2020-03-14 00:08:43 -0400

    Fix handling of comic 404

--- src/xkcd.rs
+++ src/xkcd.rs
@@ -45,12 +45,32 @@ pub fn get_latest() -> Comic {
 }
 
 pub fn get_by_id(id: i32) -> Comic {
-    let response = CallBuilder::get().timeout_ms(5000).url(&(format!("https://xkcd.com/{}/info.0.json", id))).unwrap().exec();
-
-    match response {
-        Ok((_response_meta, data)) => {
-            let latest_comic: ApiComic = serde_json::from_str(&String::from_utf8(data).unwrap()).unwrap();
-    
+    match CallBuilder::get().timeout_ms(5000).url(&(format!("https://xkcd.com/{}/info.0.json", id))).unwrap().exec() {
+        Ok((_response_meta, body)) => {
+            let body_content = match String::from_utf8(body) {
+                Ok(obj) => obj,
+                Err(_) => { panic!("No body returned"); }
+            };
+            let latest_comic: ApiComic = match serde_json::from_str(&body_content) {
+                Ok(obj) => obj,
+                Err(msg) => {
+                    if body_content.contains("404 Not Found") {
+                        return Comic {
+                            num: 404,
+                            title: String::from("404"),
+                            alt_text: String::from("404 Not Found"),
+                            transcript: String::from("The comic for this day is just a 404 page"),
+                            img: String::from("https://xkcd.com/s/0b7742.png"),
+                            year: 2008,
+                            month: 4,
+                            day: 1,
+                        } 
+                    }
+                    else {
+                        panic!("Error parsing json for {}: {}", id, msg);
+                    }
+                }
+            };
             return Comic {
                 num: latest_comic.num,
                 title: latest_comic.safe_title,
@@ -63,22 +83,7 @@ pub fn get_by_id(id: i32) -> Comic {
             }
         },
         Err(msg) => {
-            println!("Error: {}", msg);
-            if id == 404 {
-                return Comic {
-                    num: 404,
-                    title: String::from("404"),
-                    alt_text: String::from("404 Not Found"),
-                    transcript: String::from("The comic for this day is just a 404 page"),
-                    img: String::from("https://xkcd.com/s/0b7742.png"),
-                    year: 2008,
-                    month: 4,
-                    day: 1,
-                } 
-            }
-            else {
-                panic!("Error retreiving comic {}", id)
-            }
+            panic!("Error with comic {}: {}", id, msg);
         }
     }
 }
